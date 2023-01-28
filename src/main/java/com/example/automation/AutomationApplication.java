c7501c69941cc99ddb8dea4947b14f72c421fb64
Sachithra Manamperi <sachithram@zone24x7.lk>
2023-01-29 00:47:22 +0530

    changged java

--- src/main/java/com/example/automation/AutomationApplication.java
+++ src/main/java/com/example/automation/AutomationApplication.java
@@ -2,10 +2,15 @@ package com.example.automation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class AutomationApplication {
 
+	@GetMapping
+	public String message(){
+		return "Welcome to Automation Code";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(AutomationApplication.class, args);
 	}
